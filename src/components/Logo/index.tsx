426dcbc9a879ffed0f98542b1ad612db6d736c59
wesley.costa <wesley.costa@opah.com.br>
2021-12-18 10:35:11 -0300

    first commit

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,6 @@
+import * as S from './styles'
+
+const Logo = () => {
+	return <S.Logo>logo</S.Logo>
+}
+export default Logo
---
+++ src/styles/global.ts
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyles = createGlobalStyle`
+	*{
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+	}
+
+	body {
+		font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif
+	}
+`
+export default GlobalStyles
