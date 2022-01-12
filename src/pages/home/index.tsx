94461b5dc5c12e90b11248671c9adb2c80c6c758
wesley.costa <wesley.costa@opah.com.br>
2022-01-12 12:06:48 -0300

    disable exhaustive-deps

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -62,6 +62,7 @@ const Home = () => {
 
 	useEffect(() => {
 		getInitialDataFromApi()
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [])
 
 	const getInitialDataFromApi = async () => {
